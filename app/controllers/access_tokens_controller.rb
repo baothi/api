d26a4cab8d2574abe7fff48611cf0e39f7612d36
baothi <baothi246@gmail.com>
2022-08-08 17:40:10 +0700

    rspec lib it run ok

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,7 +1,7 @@
 class AccessTokensController < ApplicationController
   skip_before_action :authorize!, only: :create
   def create
-    authenticator = UserAuthenticator.new(params[:code])
+    authenticator = UserAuthenticator.new(authentication_params)
     authenticator.perform
 
     render json: authenticator.access_token, status: :created
@@ -11,4 +11,12 @@ class AccessTokensController < ApplicationController
     # raise AuthorizationError
     current_user.access_token.destroy
   end
+
+
+  private
+
+  def authentication_params
+    params.permit(:code).to_h.symbolize_keys
+  end
+  
 end
--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -23,9 +23,9 @@ RSpec.describe AccessTokensController, type: :controller do
       #   end
       # end
 
-      context 'when no code provided' do
+      context 'when no auth_data provided' do
         subject { post :create }
-        it_behaves_like "unauthorized_requests"
+        it_behaves_like "unauthorized_standard_requests"
       end
     
       context 'when invalid code provided' do
@@ -40,7 +40,7 @@ RSpec.describe AccessTokensController, type: :controller do
   
         subject { post :create, params: { code: 'invalid_code' } }
 
-        it_behaves_like "unauthorized_requests"
+        it_behaves_like "unauthorized_oauth_requests"
       end
   
       context 'when success request' do
